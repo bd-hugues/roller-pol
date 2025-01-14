81be39bde5ca022c0adcad72406c7a3c60cf55f8
bd-hugues <100399627+bd-hugues@users.noreply.github.com>
2025-01-14 09:45:54 +0400

    Update CacheManager.java

--- app/src/main/java/org/apache/roller/weblogger/util/cache/CacheManager.java
+++ app/src/main/java/org/apache/roller/weblogger/util/cache/CacheManager.java
@@ -156,7 +156,7 @@ public final class CacheManager {
             }
         }
         
-        if(cache == null) {
+        if(cache == null && properties != null) {
             // ask our default cache factory for a new cache instance
             cache = cacheFactory.constructCache(properties);
         }
